93f3a04bfc8888852383930d7dd0dcea95627d71
Abhishek Pratap <abhishek.vit@gmail.com>
2016-09-12 16:51:31 -0700

    factored out medication choice question

--- R/commonFunctions.R
+++ R/commonFunctions.R
@@ -1,19 +1,5 @@
 ######## utility functions
 
-
-process_medicationChoiceAnswers <- function(json_file) {
-  tryCatch({
-    d <- jsonlite::fromJSON(json_file)
-    data.frame(medication = paste(unique(d$identifier),
-                                  collapse = "+"), medicationTime = paste(unique(d$answer),
-                                                                          collapse = "+"))
-  }, error = function(err) {
-    data.frame(medication = "NA", medicationTime = "NA")
-  })
-}
-
-
-
 synapse_downLoadTableFile <- function(table, row, column){
   tryCatch({
     return(synDownloadTableFile(table, row, column))
