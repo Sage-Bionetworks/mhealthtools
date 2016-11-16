2f4290bd25772038c83bf79d721ee58ea5d01c09
Abhi Pratap <apratap@charon-vpn-010027.fhcrc.org>
2016-11-16 02:06:07 -0800

    added medication choice answers - replace separaters from + to ,

--- R/medicationChoices.R
+++ R/medicationChoices.R
@@ -2,8 +2,8 @@ processMedicationChoiceAnswers <- function(json_file) {
   tryCatch({
     d <- jsonlite::fromJSON(json_file)
     data.frame(medication = paste(unique(d$identifier),
-                                  collapse = "+"), medicationTime = paste(unique(d$answer),
-                                                                          collapse = "+"))
+                                  collapse = ", "), medicationTime = paste(unique(d$answer),
+                                                                          collapse = ", "))
   }, error = function(err) {
     data.frame(medication = "NA", medicationTime = "NA")
   })
