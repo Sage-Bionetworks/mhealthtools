d1f347dec20d9713fc3b8fd796346a690803698b
Meghasyam Tummalacherla <meghasyam@sagebase.org>
2018-06-19 14:29:59 -0700

    Update getHR.R

--- R/getHR.R
+++ R/getHR.R
@@ -19,12 +19,15 @@ getHR <- function(dat, windowLen = 10, freqRange = c(1,25), bpforder = 128){
   # Main Code Block
   #############################################################
 
+  dat1 = data.frame(red = NA, green = NA, blue = NA, error = NA, samplingRate = NA)
+
   # Get sampling rate
   samplingRate = tryCatch({ length(dat$timestamp)/(dat$timestamp[length(dat$timestamp)] - dat$timestamp[1]) }, 
                           error = function(e){ NA })
   if(!is.finite(samplingRate)){dat1$error = 'Sampling Rate calculated from timestamp is Inf or NaN / timestamp not found in json'; return(dat1) }
+  
   if(samplingRate < 55){if(samplingRate > 22){bpforder = 64}else{bpforder = 32}}
-  dat1$samplingRate = samplingRate
+
   # Convert window length from seconds to samples
   windowLen = round(samplingRate*windowLen)
   
